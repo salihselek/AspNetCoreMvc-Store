f0d1c0b12f0f666e72f39b9cc4bb750148cbbb3b
Salih SELEK <salihselek42@gmail.com>
2023-10-11 21:32:29 +0300

    filter by categoryId

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,4 +1,6 @@
 using Entities.Models;
+using Entities.RequestParameters;
+using Microsoft.EntityFrameworkCore;
 using Repositories.Contracts;
 
 namespace Repositories
@@ -17,6 +19,13 @@ namespace Repositories
         }
 
         public IQueryable<Product> GetAllProducts(bool trackChanges) => FindAll(trackChanges);
+
+        public IQueryable<Product> GetAllProductsWithDetails(ProductRequestParameters p)
+        {
+            return p.CategoryId is null
+                ? _context.Products.Include(prd => prd.Category)
+                : _context.Products.Include(prd => prd.Category).Where(prd => prd.CategoryId.Equals(p.CategoryId));
+        }
 
         public Product? GetOneProduct(int id, bool trackChanges)
         {
