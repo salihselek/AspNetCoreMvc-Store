75c317b7067052d3b23627e6f64ba0baa7c7bcf1
Salih SELEK <salihselek42@gmail.com>
2023-10-12 15:45:07 +0300

    lastest product tag helper created.

--- Services/ProductManager.cs
+++ Services/ProductManager.cs
@@ -45,6 +45,14 @@ namespace Services
             return _manager.Product.GetAllProductsWithDetails(p);
         }
 
+        public IEnumerable<Product> GetLastestProducts(int n, bool trackChanges)
+        {
+            return _manager.Product
+                .FindAll(trackChanges)
+                .OrderByDescending(p => p.Id)
+                .Take(n);
+        }
+
         public Product? GetOneProduct(int id, bool trackChanges)
         {
             var product = _manager.Product.GetOneProduct(id, trackChanges);
---
+++ StoreApp/Infrastructe/TagHelpers/LastestProductTagHelper.cs
@@ -0,0 +1,51 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Services.Contracts;
+
+namespace StoreApp.Infrastructe.TagHelpers
+{
+    [HtmlTargetElement("div", Attributes = "products")]
+    public class LastestProductTagHelper : TagHelper
+    {
+        private readonly IServiceManager _manager;
+
+        public LastestProductTagHelper(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            TagBuilder div = new TagBuilder("div");
+            div.Attributes.Add("class", "my-3");
+
+            TagBuilder h6 = new TagBuilder("h6");
+            h6.Attributes.Add("class", "lead");
+
+            TagBuilder icon = new TagBuilder("i");
+            icon.Attributes.Add("class", "fa fa-box text-secondary");
+
+            TagBuilder ul = new TagBuilder("ul");
+
+            var products = _manager.ProductService.GetLastestProducts(5, false);
+            foreach (var product in products)
+            {
+                TagBuilder li = new TagBuilder("li");
+                TagBuilder a = new TagBuilder("a");
+                a.Attributes.Add("href", $"/product/get/{product.Id}");
+                a.InnerHtml.AppendHtml(product.Name);
+
+                li.InnerHtml.AppendHtml(a);
+                ul.InnerHtml.AppendHtml(li);
+            }
+
+            h6.InnerHtml.AppendHtml(icon);
+            h6.InnerHtml.AppendHtml(" Lastest Products");
+
+            div.InnerHtml.AppendHtml(h6);
+            div.InnerHtml.AppendHtml(ul);
+
+            output.Content.AppendHtml(div);
+        }
+    }
+}
