904502b34559fe25360ceb9ad5e66817497af862
Salih SELEK <salihselek42@gmail.com>
2023-10-18 16:36:50 +0300

    createuser

---
+++ Entities/Dtos/UserDto.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Dtos
+{
+    public record UserDto
+    {
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "UserName is required")]
+        public string? UserName { get; init; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Email is required")]
+        public string? Email { get; init; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "PhoneNumber is required")]
+        public string? PhoneNumber { get; init; }
+
+        public HashSet<string> Roles { get; set; } = new HashSet<string>();
+    }
+}
