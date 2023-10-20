52fe2d8623a1b4ae4f70c435a9bceccf55a10926
Salih SELEK <salihselek42@gmail.com>
2023-10-20 15:09:20 +0300

    access denied and application cookie

--- StoreApp/Controllers/AccountController.cs
+++ StoreApp/Controllers/AccountController.cs
@@ -81,5 +81,10 @@ namespace StoreApp.Controllers
             }
             return View();
         }
+
+        public IActionResult AccessDenied([FromQuery(Name = "ReturnUrl")] string returnUrl)
+        {
+            return View();
+        }
     }
 }
--- StoreApp/Infrastructure/Extensions/ServiceExtension.cs
+++ StoreApp/Infrastructure/Extensions/ServiceExtension.cs
@@ -1,6 +1,8 @@
 using Entities.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using Repositories;
 using Repositories.Contracts;
 using Services;
@@ -63,6 +65,17 @@ namespace StoreApp.Infrastructure.Extensions
             services.AddScoped<ICategoryService, CategoryManager>();
             services.AddScoped<IOrderService, OrderManager>();
             services.AddScoped<IAuthService, AuthManager>();
+        }
+
+        public static void ConfigureApplicationCookie(this IServiceCollection services)
+        {
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = new PathString("/Account/Login");
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(10);
+                options.AccessDeniedPath = new PathString("/Account/AccessDenied");
+            });
         }
 
         public static void ConfigureRouting(this IServiceCollection services)
