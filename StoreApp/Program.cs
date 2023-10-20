774160fd774116f8d7731854749e847a8fbf7865
Salih SELEK <salihselek42@gmail.com>
2023-10-20 19:09:55 +0300

    presentation layer created for api

--- StoreApp/Program.cs
+++ StoreApp/Program.cs
@@ -2,6 +2,9 @@ using StoreApp.Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
+    
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
@@ -37,6 +40,8 @@ app.UseEndpoints(endpoints =>
         pattern: "{controller=Home}/{action=Index}/{id?}"
     );
     endpoints.MapRazorPages();
+
+    endpoints.MapControllers();
 });
 
 app.ConfigureAndCheckMigration();
